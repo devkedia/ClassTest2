5707ee91fa42840d685c7b0f642988e52b334926
devkedia <30996590+devkedia@users.noreply.github.com>
2017-10-10 16:29:42 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/devanand/classtest2/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.devanand.classtest2;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder mRecorder = null;
+    private Button start_button,stop_button;
+    private TextView output;
+    private double value;
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private Handler ha = null;
+    private String snor = "Silence";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start_button = (Button) findViewById(R.id.start_button);
+        stop_button = (Button) findViewById(R.id.stop_button);
+        output = (TextView) findViewById(R.id.textView);
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    start_audio();
+                    output.setText(Double.toString(value));
+                    ha=new Handler();
+                    ha.postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            //call function
+                            setAmplitude();
+                            output.setText(Double.toString(value));
+                            if (value < 10000){
+                                output.append("\nSilence");
+                            }
+                            else if (value>10000 &&value<30000){
+                                output.append("\nSnoring");
+                            }
+                            else{
+                                output.append("\nLoud Talking");
+                            }
+                            ha.postDelayed(this, 1000);
+                        }
+                    }, 1000);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        stop_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stop_audio();
+                output.setText(Double.toString(value));
+            }
+        });
+
+
+
+    }
+    public void start_audio() throws IOException {
+        if (mRecorder == null) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            mRecorder.prepare();
+            mRecorder.start();
+
+            value = getAmplitude();
+        }
+    }
+
+    public void stop_audio() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+            value = -1;                                 // Set as default value for no input
+        }
+    }
+
+    public void setAmplitude(){
+        value = getAmplitude();
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  mRecorder.getMaxAmplitude();
+        else
+            return 0;
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+}
